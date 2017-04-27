154198d711aa437bc5443c1ad61e9ebda3174db3
Ubuntu <ubuntu@ip-172-31-21-255.eu-west-1.compute.internal>
2017-04-27 18:52:25 +0000

    Read_Preprocess_data program finished with reading data from excel file and loading corresponding image and storing things as a list of objects

--- param_global.py
+++ param_global.py
@@ -1,5 +1,27 @@
 # Parameters File
 import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings('ignore')
+
 astrohack_path=os.path.dirname(os.path.realpath(__file__))
+
+data_path=os.path.join(astrohack_path,"Data")
+sample_data_path=os.path.join(data_path,"SAMPLE")
+
+
+code_path=os.path.join(astrohack_path,"Code")
+neural_network_path=os.path.join(code_path,"Neural_Network")
+pre_processing_path=os.path.join(code_path,"Pre_processing")
+predictions_path=os.path.join(code_path,"Predicitons")
+
+
+
+
+
+
+
 
 
