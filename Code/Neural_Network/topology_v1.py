37b680a0090ab204768d418662e688dc1868295f
Thomas Carette <tcarette@gmail.com>
2017-04-29 23:53:17 +0200

    factor out net topology

---
+++ Code/Neural_Network/topology_v1.py
@@ -0,0 +1,35 @@
+# Keras model
+from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D
+from keras.models import Model
+from keras import backend as K
+
+input_img = Input(shape=(128, 128, 1))  # adapt this if using `channels_first` image data format
+
+ws = 3
+mpw = 2
+
+x = Conv2D(32, (ws,ws), activation='relu', padding='same')(input_img)
+print("1st Convolutional layer shape",x.shape)
+x = MaxPooling2D((mpw,mpw), padding='same')(x)
+print("1st Maxpooling layer shape",x.shape)
+x = Conv2D(16, (ws,ws), activation='relu', padding='same')(x)
+print("2nd Convolutional layer shape",x.shape)
+x = MaxPooling2D((mpw,mpw), padding='same')(x)
+print("2nd Maxpooling layer shape",x.shape)
+x = Conv2D(16, (ws,ws), activation='relu', padding='same')(x)
+print("3rd Convolutional layer shape",x.shape)
+encoded = MaxPooling2D((mpw,mpw), padding='same')(x)
+print("Encoding layer shape",x.shape)
+
+# at this point the representation is (4, 4, 8) i.e. 128-dimensional
+
+x = Conv2D(16, (ws,ws), activation='relu', padding='same')(encoded)
+x = UpSampling2D((mpw,mpw))(x)
+x = Conv2D(16, (ws,ws), activation='relu', padding='same')(x)
+x = UpSampling2D((mpw,mpw))(x)
+x = Conv2D(32, (ws,ws), activation='relu', padding='same')(x)
+x = UpSampling2D((mpw,mpw))(x)
+
+decoded = Conv2D(1, (ws,ws), activation='sigmoid', padding='same')(x)
+
+print("Executing topology v1")
