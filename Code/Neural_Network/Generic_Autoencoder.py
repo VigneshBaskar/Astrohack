1039ee18e9d58f644586fd43acd3e6e69f918033
Thomas Carette <tcarette@gmail.com>
2017-04-29 16:54:21 +0200

    change autoencoder to v2

--- Code/Neural_Network/Generic_Autoencoder.py
+++ Code/Neural_Network/Generic_Autoencoder.py
@@ -54,15 +54,15 @@ x = Conv2D(16, (ws,ws), activation='relu', padding='same')(x)
 print("2nd Convolutional layer shape",x.shape)
 x = MaxPooling2D((mpw,mpw), padding='same')(x)
 print("2nd Maxpooling layer shape",x.shape)
-#x = Conv2D(16, (ws,ws), activation='relu', padding='same')(x)
-#print("3rd Convolutional layer shape",x.shape)
-#encoded = MaxPooling2D((mpw,mpw), padding='same')(x)
-#print("Encoding layer shape",x.shape)
+x = Conv2D(16, (ws,ws), activation='relu', padding='same')(x)
+print("3rd Convolutional layer shape",x.shape)
+encoded = MaxPooling2D((mpw,mpw), padding='same')(x)
+print("Encoding layer shape",x.shape)
 
 # at this point the representation is (4, 4, 8) i.e. 128-dimensional
 
-#x = Conv2D(16, (ws,ws), activation='relu', padding='same')(encoded)
-#x = UpSampling2D((mpw,mpw))(x)
+x = Conv2D(16, (ws,ws), activation='relu', padding='same')(encoded)
+x = UpSampling2D((mpw,mpw))(x)
 x = Conv2D(16, (ws,ws), activation='relu', padding='same')(x)
 x = UpSampling2D((mpw,mpw))(x)
 x = Conv2D(32, (ws,ws), activation='relu', padding='same')(x)
@@ -99,6 +99,6 @@ decoded_imgs = autoencoder.predict(X_train)
 
 ### Save results
 
-with open(os.path.join(output_path,"autoencoder_results_v1_"+ "_".join(selectedBatches)), 'wb') as handle:
+with open(os.path.join(output_path,"autoencoder_results_v2_"+ "_".join(selectedBatches)), 'wb') as handle:
   pickle.dump(decoded_imgs, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
