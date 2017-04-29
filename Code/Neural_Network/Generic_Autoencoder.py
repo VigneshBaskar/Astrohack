cedb9528a419c971df589f6969e1a1e4631ab876
Thomas Carette <tcarette@gmail.com>
2017-04-29 22:01:06 +0200

    m

--- Code/Neural_Network/Generic_Autoencoder.py
+++ Code/Neural_Network/Generic_Autoencoder.py
@@ -14,9 +14,9 @@ from Data_Preparation_Library import *
 
 sys.path.append(pre_processing_path)
 
-#selectedBatches=["6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"]
-maxBatchId = 1
-selectedBatches=[str(i) for i in range(maxBatchId)]
+selectedBatches=["6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"]
+#maxBatchId = 
+#selectedBatches=[str(i) for i in range(maxBatchId)]
 batch_data_object = []
 for i in selectedBatches:
 	with open(os.path.join(temp_path,'full_data_object_' + i + '.p'), 'rb') as handle:
@@ -55,15 +55,15 @@ x = Conv2D(16, (ws,ws), activation='relu', padding='same')(x)
 print("2nd Convolutional layer shape",x.shape)
 x = MaxPooling2D((mpw,mpw), padding='same')(x)
 print("2nd Maxpooling layer shape",x.shape)
-x = Conv2D(16, (ws,ws), activation='relu', padding='same')(x)
-print("3rd Convolutional layer shape",x.shape)
-encoded = MaxPooling2D((mpw,mpw), padding='same')(x)
-print("Encoding layer shape",x.shape)
+#x = Conv2D(16, (ws,ws), activation='relu', padding='same')(x)
+#print("3rd Convolutional layer shape",x.shape)
+#encoded = MaxPooling2D((mpw,mpw), padding='same')(x)
+#print("Encoding layer shape",x.shape)
 
 # at this point the representation is (4, 4, 8) i.e. 128-dimensional
 
-x = Conv2D(16, (ws,ws), activation='relu', padding='same')(encoded)
-x = UpSampling2D((mpw,mpw))(x)
+#x = Conv2D(16, (ws,ws), activation='relu', padding='same')(encoded)
+#x = UpSampling2D((mpw,mpw))(x)
 x = Conv2D(16, (ws,ws), activation='relu', padding='same')(x)
 x = UpSampling2D((mpw,mpw))(x)
 x = Conv2D(32, (ws,ws), activation='relu', padding='same')(x)
@@ -105,15 +105,15 @@ encoded_imgs_train = encoder.predict(X_train)
 ### Save results
 
 #with open(os.path.join(output_path,"autoencoder_results_v3_up_to_" + str(maxBatchId) ), 'wb') as handle:
-with open(os.path.join(output_path,"autoencoder_results_test_v2_" + "_".join(selectedBatches) ), 'wb') as handle:
+with open(os.path.join(output_path,"autoencoder_results_test_v1_" + "_".join(selectedBatches) ), 'wb') as handle:
   pickle.dump(decoded_imgs_test, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-with open(os.path.join(output_path,"autoencoder_results_train_v2_" + "_".join(selectedBatches) ), 'wb') as handle:
+with open(os.path.join(output_path,"autoencoder_results_train_v1_" + "_".join(selectedBatches) ), 'wb') as handle:
   pickle.dump(decoded_imgs_train, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-with open(os.path.join(output_path,"encoder_results_test_v2_" + "_".join(selectedBatches) ), 'wb') as handle:
+with open(os.path.join(output_path,"encoder_results_test_v1_" + "_".join(selectedBatches) ), 'wb') as handle:
   pickle.dump(encoded_imgs_test, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-with open(os.path.join(output_path,"encoder_results_train_v2_" + "_".join(selectedBatches) ), 'wb') as handle:
+with open(os.path.join(output_path,"encoder_results_train_v1_" + "_".join(selectedBatches) ), 'wb') as handle:
   pickle.dump(encoded_imgs_train, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
